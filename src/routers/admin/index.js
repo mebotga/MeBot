de92a60414ac8353eba154afbf285c91845291a2
mebotga <95586442+mebotga@users.noreply.github.com>
2021-12-11 22:33:46 +0300

    Update index.js

--- src/routers/admin/index.js
+++ src/routers/admin/index.js
@@ -7,7 +7,6 @@ let sitedatalari = require("../../database/models/analytics-site.js");
 
 console.log("[mebot.ga]: Admin/Index router loaded.");
 app.get("/admin", global.checkAuth, async (req, res) => {
-	const botdata = await botsdata.find();
 	const codedata = await codesSchema.find();
 	const udata = await uptimedata.find();
 	let sites = await sitedatalari.findOne({ id: config.website.clientID });
@@ -25,107 +24,6 @@ app.get("/admin", global.checkAuth, async (req, res) => {
         udata: udata, 
         siteD: sites,
         serverdata:server
-    })
-});
-
-app.get("/admin/unapproved", global.checkAuth, async (req, res) => {
-	const botdata = await botsdata.find();
-    res.render("admin/unapproved.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata, 
-    })
-});
-app.get("/admin/approved", global.checkAuth, async (req, res) => {
-	const botdata = await botsdata.find();
-    res.render("admin/approved.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata, 
-    })
-});
-app.get("/admin/certificate-apps", checkAuth, async (req, res) => {
-	const botdata = await botsdata.find();
-    const apps = await appsdata.find()
-    res.render("admin/certificate-apps.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata, 
-        apps: apps
-    })
-});
-app.get("/admin/report-apps", checkAuth, async (req, res) => {
-	const botdata = await botsdata.find();
-    const apps = await reportappsdata.find()
-    res.render("admin/report-app.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata, 
-        apps: apps
-    })
-});
-app.get("/admin/decline/:botID", global.checkAuth, async (req, res) => {
-    const botdata = await botsdata.findOne({ botID: req.params.botID })
-    if(!botdata) return res.redirect("/error?code=404&message=You entered an invalid bot id.");
-    res.render("admin/decline.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata
-    })
-});
-
-app.get("/admin/certified-bots", global.checkAuth, async (req, res) => {
-    const botdata = await botsdata.find();
-    res.render("admin/certified-bots.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata
-    })
-});
-app.get("/admin/certificate/delete/:botID", global.checkAuth, async (req, res) => {
-    const botdata = await botsdata.findOne({
-        botID: req.params.botID
-    })
-    if (!botdata) return res.redirect("/error?code=404&message=You entered an invalid bot id.");
-    res.render("admin/certificate-delete.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        roles:global.config.server.roles,
-        channels: global.config.server.channels,
-        botdata: botdata
     })
 });
 module.exports = app;
