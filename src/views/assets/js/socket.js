15e6fd772ee75e31d11cee334162b5733d4a2519
mebotga <95586442+mebotga@users.noreply.github.com>
2021-12-12 10:50:43 +0300

    Delete src/views directory

--- src/views/assets/js/socket.js
+++
@@ -1,8 +0,0 @@
-const socket = io.connect("https://mebot.ga");
-
-socket.on('userCount', userCount => {
-let doc = document.getElementById('connectionCount');
-  if(doc) {
-    doc.innerHTML = userCount;
-  }
-})
